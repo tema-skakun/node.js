b7af7f14b241887c5c872f3d60148fd0e7ecdeb8
tema-skakun <frederico_f@mail.ru>
2024-02-29 22:55:17 +0100

    feat: creates server

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-const Logger = require('./log');
+// const Logger = require('./log');
+//
+// const logger = new Logger();
+//
+// logger.on('some_event', (args) => {
+//   const { id, text } = args;
+//   console.log(id, text);
+// });
+//
+// logger.log('User Logged!');
 
-const logger = new Logger();
+const http = require('http');
 
-logger.on('some_event', (args) => {
-  const { id, text } = args;
-  console.log(id, text);
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log('Server request');
+  console.log(req.url, req.method);
+
+  res.setHeader('Content-Type', 'text/html');
+  // res.setHeader('Content-Type', 'application/json');
+
+  // const data = JSON.stringify([
+  //   { name: 'Tommy', age: 35 },
+  //   { name: 'Arthur', age: 40 },
+  // ]);
+
+  res.write('<head><link rel="stylesheet" href="#"></head>');
+  res.write('<h1>Hello world!</h1>');
+  res.write('<p>My name is Artem</p>');
+
+  res.end();
 });
 
-logger.log('User Logged!');
+server.listen(PORT, 'localhost', (error) => {
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
