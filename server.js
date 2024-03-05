1454f996d61aaf0b21b1dda110cd8d3ce6fb704e
tema-skakun <frederico_f@mail.ru>
2024-03-05 23:33:20 +0100

    feat: adds handling post requests

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.listen(PORT, (error) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static('styles'));
 
 app.get('/', (req, res) => {
@@ -35,12 +37,40 @@ app.get('/contacts', (req, res) => {
 
 app.get('/posts/:id', (req, res) => {
   const title = 'Post';
-  res.render(createPath('post'), { title });
+  const post = {
+    id: '1',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
+    title: 'Post title',
+    date: '3/4/2024',
+    author: 'Artem',
+  };
+  res.render(createPath('post'), { title, post });
 });
 
 app.get('/posts', (req, res) => {
   const title = 'Posts';
-  res.render(createPath('posts'), { title });
+  const posts = [
+    {
+      id: '1',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
+      title: 'Post title',
+      date: '3/4/2024',
+      author: 'Artem',
+    }
+  ];
+  res.render(createPath('posts'), { title, posts });
+});
+
+app.post('/add-post', (req, res) => {
+  const { title, author, text } = req.body;
+  const post = {
+    id: new Date(),
+    date: (new Date()).toLocaleDateString(),
+    title,
+    author,
+    text,
+  };
+  res.render(createPath('post'), { post, title });
 });
 
 app.get('/add-post', (req, res) => {
